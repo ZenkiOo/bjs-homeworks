4e121d51211726f949aed2898e44bc50c918d8c4
ZenkiOo <zenky@list.ru>
2020-09-03 00:55:31 +0300

    1.2-extended-syntax answer

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,48 @@
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+"use strict";
+
+function getResult(a, b, c) {
+  let x1, x2, result;
+  let x = [];
+  let D = Math.pow(b, 2) - 4 * a * c;
+
+  if (D > 0) {
+    x1 = (-b + Math.sqrt(D)) / (2 * a);
+    x2 = (-b - Math.sqrt(D)) / (2 * a);
+    x.push(x1, x2);
+    result = `В квадратном уравнении два корня: x₁ = ${x[0]}, x₂ = ${x[1]}`;
+    return (x = result);
+  } else if (D === 0) {
+    x1 = -b / (2 * a);
+    x.push(x1);
+    result = `В квадратном уравнении один корень: x = ${x}`;
+    return (x = result);
+  } else if (D < 0) {
+    result = "В квадратном уравнении корней нет т.к. D < 0";
+    return (x = result);
+  }
 }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+function getAverageMark(marks) {
+  let averageMark = 0,
+    marksToCount = marks.slice(0, 5);
+
+  if (marks.length > 0) {
+    if (marks.length > 5) console.log("Количество оценок больше 5");
+    for (let i = 0; i < marksToCount.length; i++) {
+      averageMark += marksToCount[i] / marksToCount.length;
+    }
+  }
+  return averageMark;
 }
 
-function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+function askDrink(name, dateOfBirthday) {
+  let now = new Date(),
+    result;
+
+  if (now.getFullYear() - dateOfBirthday.getFullYear() >= 18) {
+    result = `Не желаете ли олд-фэшн, ${name}?`;
+  } else {
+    result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+  }
+  return result;
 }
