fcea90c2bf7df5658b7c5bb13e9bb2d45da7a2f6
ZenkiOo <zenky@list.ru>
2020-09-12 17:01:56 +0300

    2.2-object-prototype answer

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,32 @@
-//String.prototype.isPalindrome - для задачи №1
+"use strict";
+
+String.prototype.isPalindrome = function isPalindrome() {
+  const splittedString = this.toLowerCase().split("");
+  for (let i = 0; i < splittedString.length; i++) {
+    if (splittedString[i] === " ") {
+      splittedString.splice([i], 1);
+    }
+  }
+  if (splittedString.join("") === splittedString.reverse().join("")) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+  let averageMark = 0;
+  for (let i = 0; i < marks.length; i++) {
+    averageMark += marks[i] / marks.length;
+  }
+  const roundedAverage = Math.round(averageMark);
+  return roundedAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+  birthday = new Date(birthday).getTime();
+  const now = new Date().getTime(),
+    diff = now - birthday,
+    age = diff / (365 * 24 * 60 * 60 * 1000 + 24 * 60 * 60 * 1000 * 0.25);
+  return age > 18 ? true : false;
 }
