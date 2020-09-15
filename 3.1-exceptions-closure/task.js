f915e8d5d9c8ae39ce05212dd29e24d25467cb6e
ZenkiOo <zenky@list.ru>
2020-09-15 13:03:44 +0300

    3.1-exceptions-closure answer

---
+++ 3.1-exceptions-closure/task.js
@@ -0,0 +1,56 @@
+"use strict";
+document.body.style.backgroundColor = "#333";
+const c = () => console.log;
+
+function parseCount(num) {
+  const number = Number.parseInt(num, 10);
+  if (Number.isNaN(number)) {
+    throw new Error("Невалидное значение");
+  }
+  return number;
+}
+
+function validateCount(val) {
+  try {
+    return parseCount(val);
+  } catch {
+    return "Невалидное значение";
+  }
+}
+
+class Triangle {
+  constructor(a, b, c) {
+    if (a + b < c || a + c < b || c + b < a) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+  getPerimeter() {
+    return this.a + this.b + this.c;
+  }
+  getArea() {
+    let p = this.getPerimeter() / 2;
+    return +Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(
+      3
+    );
+  }
+}
+// const tr = new Triangle(6, 5, 2.2);
+
+function getTriangle(a, b, c) {
+  const errorMessage = "Ошибка! Треугольник не существует";
+  try {
+    return new Triangle(a, b, c);
+  } catch {
+    return {
+      getArea() {
+        return errorMessage;
+      },
+      getPerimeter() {
+        return errorMessage;
+      },
+    };
+  }
+}
